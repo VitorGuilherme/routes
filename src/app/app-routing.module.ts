45b440881e8c91b8786d9a81dcfa8a330c6f5b54
VitorGuilherme <alfredo.neg9@gmail.com>
2022-09-08 23:46:52 -0300

    routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
+import { SobreComponent } from './pages/sobre/sobre.component';
+import { PageErrorComponent } from './pages/page-error/page-error.component';
+import { HomeComponent } from './pages/home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  // { path: "", component: PageHomeComponent, pathMatch: 'full'},
-  // { path: "sobre", component: PageSobreComponent}
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: 'sobre/:id/:username', component: SobreComponent },
+  { path: '404', component: PageErrorComponent },
+  { path: '**', redirectTo: '404' }
 ];
 
 @NgModule({
