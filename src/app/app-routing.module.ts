d0212a4205f01cfff7c81522a5173cb6be2ac412
VitorGuilherme <alfredo.neg9@gmail.com>
2022-09-14 11:26:18 -0300

    Fim das aulas sobre rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,13 +6,18 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
-  { path: 'sobre/:id/:username', component: SobreComponent },
+  {
+    path: 'sobre', component: SobreComponent, children: [
+      { path: 'vitor', component: SobreComponent }
+    ]
+  },
+  { path: 'dashboard', loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule) },
   { path: '404', component: PageErrorComponent },
   { path: '**', redirectTo: '404' }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {useHash: true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
